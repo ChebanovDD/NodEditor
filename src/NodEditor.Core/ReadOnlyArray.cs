0802a9ee30051ae6e060ce674d604bdfcc33e571
ChebanovDD <chebanovdd@gmail.com>
2022-10-25 20:38:05 +0700

    Add EmptyReadOnlyArray.

--- src/NodEditor.Core/ReadOnlyArray.cs
+++ src/NodEditor.Core/ReadOnlyArray.cs
@@ -1,16 +1,28 @@
-﻿namespace NodEditor.Core
+﻿using System;
+
+namespace NodEditor.Core
 {
     public class ReadOnlyArray<T>
     {
         private readonly T[] _items;
-        
+
         public ReadOnlyArray(T[] items)
         {
             _items = items;
             Length = items.Length;
         }
-        
+
         public int Length { get; }
         public T this[int index] => _items[index];
+
+        public static ReadOnlyArray<T> Empty()
+        {
+            return EmptyReadOnlyArray.Value;
+        }
+
+        private static class EmptyReadOnlyArray
+        {
+            internal static readonly ReadOnlyArray<T> Value = new(Array.Empty<T>());
+        }
     }
 }
